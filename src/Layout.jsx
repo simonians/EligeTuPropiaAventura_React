b4f38e9980bb082d9cf65b64a50d64ddc3074fed
simonians <simonsimonian37@gmail.com>
2021-09-08 19:25:34 -0300

    Agrego modificaciones en los componentes Layout y Records

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,28 +2,43 @@ import { Component } from "react";
 import Records from "./Records";
 import Options from "./Options.jsx";
 import data from "./components/data.json"
+import { queryAllByAltText } from "@testing-library/react";
 
 
 export default class Layout extends Component {
     constructor(props){
         super(props)
         this.state = {
-            history: [],
             contador: 0,
-            selectedOption: ""
+            selectedOption: "",
         }
+        this.history = []
         this.handleClick = this.handleClick.bind(this)
     }
 
-    handleClick = (op) => {
-        const sum = op === "A" ? 1 : 2;
+    componentDidUpdate(prevState) {
+        if (prevState.contador !== this.state.contador) {
+            console.log(this.history);
+            this.history.push(this.state.selectedOption)
+        }
+      }
+
+    handleClick(op) {
+        if(this.state.contador>=7){
+            alert("FIN!");
+            return;
+        }
+
+        let sum = op === "A" ? 1 : 2;
         const option = op === "A" ? "A" : "B";
-        const newHistory = this.state.history.push(option)
-        console.log(option, newHistory);
+
+        if(this.history.length > 0){
+            sum = 2;
+        }
+
         this.setState ({
             contador: this.state.contador + sum,
-            selectedOption: option
-            //history: newHistory
+            selectedOption: option,
         })
     }
 
@@ -32,7 +47,7 @@ export default class Layout extends Component {
             <div className="layout">
                 <h1 className="historia">{data[this.state.contador].historia}</h1>
                 <Options optionA={data[this.state.contador].opciones.a} optionB={data[this.state.contador].opciones.b} handleClick={this.handleClick} />
-                <Records selectedOption={this.state.selectedOption}/>
+                <Records selectedOption={this.state.selectedOption} history={this.history}/>
             </div>
         )
     }
--- src/Records.jsx
+++ src/Records.jsx
@@ -1,12 +1,14 @@
 import { Component } from "react";
 
 export default class Records extends Component {
-    render(){
+    render() {
         return (
             <div className="recordatorio">
                 <h3>Selección anterior: {this.props.selectedOption}</h3>
                 <h4>Historial de opciones elegidas: </h4>
-                <ul></ul>
+                <ul>{
+                    this.props.history.map((record, index) => <li key={index}>{record}</li> )
+                }</ul>
             </div>
         )
     }
